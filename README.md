6b403358548fb04158fca29733eca5b5b8996502
Christopher Morgan <cjmorgan1185@gmail.com>
2018-05-06 21:02:11 +0000

    sequence function crearted

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
+#Memory Game
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
-
-
-Hi there! Welcome to Cloud9 IDE!
-
-To get you started, create some files, play with the terminal,
-or visit http://docs.c9.io for our documentation.
-If you want, you can also go watch some training videos at
-http://www.youtube.com/user/c9ide.
-
-Happy coding!
-The Cloud9 IDE team
+##Logic
+first created a function that animated the box of it wwas selected.
+next created a fucntion that random selected a box and animated it if it was selected. the function frist generated a number between 0-3, it then antimate the box that corresponded to the element from an array of boxes
--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,36 @@
 $(document).ready(function() {
-    var boxes = ["red","blue","green","yellow"]
-    
-    $(".box").click(function() {
-        $(this).animate({opacity: "0.5"}, "slow");
-        $(this).animate({opacity: "1"}, "slow");
+    var boxes = ["red", "blue", "green", "yellow"];
+    var i;
+    var sequence = Array();
+    var d = 0;
+
+    $(".start-button").click(function() {
+        i = Math.floor((Math.random() * 4));
+        var selected = "#" + boxes[i] + "-box";
+
+        function buildSequence() {
+            sequence[d] = selected;
+            d++;
+        }
+
+        $(selected).animate({ opacity: "0.5" }, "slow");
+        $(selected).animate({ opacity: "1" }, "slow");
+
+
+        buildSequence();
+        
+        console.log(boxes[i]);
+        console.log(sequence);
     });
-    
-    $(".start-button").click(function sequence(){
-        var random = Math.floor((Math.random()*4));
-        $("#" + boxes[random] + "-box").animate({opacity: "0.5"}, "slow");
-        $("#" + boxes[random] + "-box").animate({opacity: "1"}, "slow")
-        console.log(boxes[random]);
-    })
-    
+
+
+
+    /* $(".box").click(function() {
+             i = Math.floor((Math.random() * 4));
+             sequence[d] = boxes[i];
+             d++;
+         });
+         
+         console.log(boxes[i]); console.log(sequence);*/
+
 });
-
-
